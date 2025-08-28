d9a5d76ae039d453302c3d105631cac5a4b2dd16
Bratth05 <brataguilar25@gmail.com>
2025-08-27 22:05:53 -0600

    Cambios Locales

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./modules/auth/pages/LoginPage";
 import RegisterOptions from "./modules/auth/pages/RegisterOptions";
 import RegisterEmprendedor from "./modules/auth/pages/RegisterEmprendedor";
+import LoginEmprendedor from "./modules/auth/pages/LoginEmprendedor";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/register" replace />} />
-
+        <Route path="/login/emprendedor" element={<LoginEmprendedor />}/>
         <Route path="/register" element={<RegisterOptions />} />
         <Route path="/register/emprendedor" element={<RegisterEmprendedor />} />
         <Route path="/register/cliente" element={<div>Registro Cliente</div>} />
